86a2901e9610e2b3c1eda630011810c699c55f2c
riki-1101 <maguro-sa-mon-riki@au.com>
2025-02-03 16:18:32 +0900

    js

--- js/footer.js
+++ js/footer.js
@@ -2,12 +2,9 @@ const footer = document.getElementById('footer');
 if( footer != null) {
     footer.innerHTML = `
     <div class="footer-menu">
-        <ul>
-            <li><a href="/math/index.html">HOME</a></li>
-            <li><a href="#">お問い合わせ</a></li>
-        </ul>
+        
     </div>
     <div class="copyright">
-        <p>math</p>
+        <p>integral</p>
     </div>`
 }
--- js/header.js
+++ js/header.js
@@ -1,12 +1,6 @@
 const header = document.getElementById('header');
 if( header != null) {
     header.innerHTML = `
-    <div><a href="/math/index.html" id="title">Mathematics</a></div>
-    <ul>
-        <li><a href="/math/calculus.html"><div class="field">calculus</div>解析学</a></li>
-        <li><a href="/math/algebra.html"><div class="field">algebra</div>代数学</a></li>
-        <li><a href="/math/probability-and-statistics.html"><div class="field">probability and statistics</div>確率と統計</a></li>
-        <li><a href="/math/set-theory.html"><div class="field">set theory</div>集合論</a></li>
-        <li><a href="/math/machine-learning.html"><div class="field">machine learning</div>機械学習</a></li>
-    </ul>`
+    <div><a href="/math/index.html" id="title">Integral</a></div>
+    `
 }
