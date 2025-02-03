f3f3236b5bcac322dda09d0adc5631c045bfa3c7
riki-1101 <maguro-sa-mon-riki@au.com>
2025-02-03 16:08:56 +0900

    first commit

---
+++ js/footer.js
@@ -0,0 +1,13 @@
+const footer = document.getElementById('footer');
+if( footer != null) {
+    footer.innerHTML = `
+    <div class="footer-menu">
+        <ul>
+            <li><a href="/math/index.html">HOME</a></li>
+            <li><a href="#">お問い合わせ</a></li>
+        </ul>
+    </div>
+    <div class="copyright">
+        <p>math</p>
+    </div>`
+}
---
+++ js/side-menu.js
@@ -0,0 +1,9 @@
+const side = document.getElementById('side-menu');
+if( side != null) {
+    side.innerHTML = `
+    <input id="search" type="search" name="search" placeholder="サイト内を検索">
+    <ul>
+        <li><a href="#"></a></li>
+        <li><a href="#"></a></li>
+    </ul>`
+}
