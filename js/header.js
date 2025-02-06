505fd9b4535b173e107551b8440e17990395fef4
riki-1101 <maguro-sa-mon-riki@au.com>
2025-02-06 13:56:47 +0900

    html,js,json

--- js/header.js
+++ js/header.js
@@ -1,6 +1,6 @@
 const header = document.getElementById('header');
 if( header != null) {
     header.innerHTML = `
-    <div><a href="/integral/index.html" id="title">Integral</a></div>
+    <div><a href="/calculus/index.html" id="title">Calculus</a></div>
     `
 }
