58fb886300f0571bc1885e71ad9bb96ccbdc2177
riki-1101 <maguro-sa-mon-riki@au.com>
2025-02-15 00:03:30 +0900

    html,css,js

--- js/header.js
+++ js/header.js
@@ -10,7 +10,7 @@ if( header != null) {
         </div>
         <ul>
             <li><a href="/calculus/index.html">ホーム</a></li>
-            <li><a href=""></a></li>
+            <li><a href="">TOPIC</a></li>
             <li><a href=""></a></li>
             <li><a href=""></a></li>
         </ul>
