84bf25ffec4595ac197c94dc8f952d6392373008
riki-1101 <maguro-sa-mon-riki@au.com>
2025-03-10 18:01:26 +0900

    safari

--- js/mathjax-config.js
+++ js/mathjax-config.js
@@ -1,3 +1,8 @@
+// Safariブラウザを判別
+function isSafari() {
+  return /Safari/.test(navigator.userAgent) && !/Chrome/.test(navigator.userAgent);
+}
+
 window.MathJax = {
     options: {
       enableMenu: false,
@@ -9,3 +14,12 @@ window.MathJax = {
       packages: { '[+]': ['ams'] }  // amsパッケージ追加
     }
 };
+
+// Safariの場合のみSVGレンダリングモードに設定
+if (isSafari()) {
+  window.MathJax.SVG = {
+    scale: 100,
+    minScale: 50,
+    linebreaks: { automatic: true }
+  };
+}
