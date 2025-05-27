63a9834eb41a23213577535d2503647c544fb995
riki-1101 <maguro-sa-mon-riki@au.com>
2025-05-28 00:15:15 +0900

    js

--- js/header.js
+++ js/header.js
@@ -1,7 +1,7 @@
 const header = document.getElementById('header');
 if( header != null) {
     header.innerHTML = `
-    <div><a href="/calculus/index.html" id="title">MathIon</a></div>
+    <div><a href="/mathion/index.html" id="title">MathIon</a></div>
     <nav>
         <div class="hamburger">
             <span></span>
@@ -9,7 +9,7 @@ if( header != null) {
             <span></span>
         </div>
         <ul>
-            <li><a href="/calculus/index.html">ホーム</a></li>
+            <li><a href="/mathion/index.html">ホーム</a></li>
             <li><a href="">TOPIC</a></li>
             <li><a href=""></a></li>
             <li><a href=""></a></li>
