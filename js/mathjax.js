a6a60cd5c739aec8ba743d40d64748e4e4178993
riki-1101 <maguro-sa-mon-riki@au.com>
2026-03-12 14:39:18 +0900

    js

--- js/mathjax.js
+++ js/mathjax.js
@@ -3,22 +3,23 @@ const isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
 
 // MathJax 設定
 window.MathJax = {
-    tex: { inlineMath: [['$', '$'], ['\\(', '\\)']] },
+    tex: {
+        inlineMath: [['$', '$'], ['\\(', '\\)']],
+        packages: { '[+]': ['ams'] }  // amsパッケージ追加
+    },
     svg: { fontCache: 'global' },
     options: {
         enableMenu: false,
         renderActions: {
             addMenu: []
         }
-    },
-    tex: {
-        packages: { '[+]': ['ams'] }  // amsパッケージ追加
     }
 };
 
 // Safari → SVG（tex-mml-svg.js）
 // それ以外 → CHTML（tex-mml-chtml.js）
 const script = document.createElement('script');
+script.defer = true;
 script.src = isSafari
   ? '../mathjax/es5/tex-mml-svg.js'
   : '../mathjax/es5/tex-mml-chtml.js';
